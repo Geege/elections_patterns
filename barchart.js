d4379876b97e999b917f92a9a33620d62f74b529
Geege <urokhard@hotmail.com>
2015-08-19 19:09:05 -0700

    Added test.html Added barchart.js Deleted unnecessary files.

---
+++ barchart.js
@@ -0,0 +1,2 @@
+    d3.select("body").append("h3").text("please work!");
+  //console.log("Yes I work. Buggey");
---
+++ elections.js
@@ -0,0 +1,3 @@
+d3.select("body").append("p").text("OLD paragraph!");
+  //console.log("Yes I work. Buggey");
+  console.log("NOW SCREW YOU");
